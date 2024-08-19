aac07ab8dfc9c603f40bfa3bf17acf642b200252
Jeff Held <jh.software.developer@gmail.com>
2024-08-19 13:30:45 +0200

    Add confirmation before deleting friend

---
+++ src/app/confirm-delete/confirm-delete.component.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  Inject,
+} from '@angular/core';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { Friend } from '../../friend';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-confirm-delete',
+  standalone: true,
+  imports: [CommonModule, MatDialogModule, MatButtonModule],
+  templateUrl: './confirm-delete.component.html',
+  styleUrl: './confirm-delete.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ConfirmDeleteComponent {
+  readonly dialogRef = inject(MatDialogRef<ConfirmDeleteComponent>);
+
+  constructor(@Inject(MAT_DIALOG_DATA) public friend: Friend) {}
+}
--- src/app/friends-list-item/friends-list-item.component.ts
+++ src/app/friends-list-item/friends-list-item.component.ts
@@ -3,6 +3,7 @@ import {
   ChangeDetectionStrategy,
   Component,
   EventEmitter,
+  inject,
   Input,
   Output,
 } from '@angular/core';
@@ -11,6 +12,8 @@ import { DateOfBirthPipe } from '../dateOfBirth.pipe';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDeleteComponent } from '../confirm-delete/confirm-delete.component';
 
 @Component({
   selector: 'app-friends-list-item',
@@ -30,4 +33,18 @@ export class FriendsListItemComponent {
   @Input({ required: true }) item!: Friend;
 
   @Output() delete = new EventEmitter<string>();
+
+  readonly dialog = inject(MatDialog);
+
+  onDeleteRequested(): void {
+    const dialogRef = this.dialog.open(ConfirmDeleteComponent, {
+      data: this.item,
+    });
+
+    dialogRef.afterClosed().subscribe((shouldDelete) => {
+      if (shouldDelete) {
+        this.delete.emit(this.item.id);
+      }
+    });
+  }
 }
